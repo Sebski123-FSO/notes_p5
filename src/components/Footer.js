cb3738baad1da41f825a23f51272a706828f1404
sebski123 <sebthomle@gmail.com>
2022-02-21 18:51:17 +0100

    part 5 d

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,10 @@ const Footer = () => {
   return (
     <div style={footerStyle}>
       <br />
-      <em>Note app, Department of Computer Science, University of Helsinki 2021</em>
+      <em>
+        Note app, Department of Computer Science, University of Helsinki
+        2022
+      </em>
     </div>
   );
 };
